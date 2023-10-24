e32450dac5821fc8f412b67c2e56f76d4f25b33e
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-10-24 12:43:15 -0300

    Adicionar arquivos de projeto.

---
+++ MembrosEstaticos2/Calculadora.cs
@@ -0,0 +1,14 @@
+﻿namespace MembrosEstaticos2 {
+    internal class Calculadora {
+
+        public static double PI = Math.PI;
+
+        public static double Circunferencia(double r) {
+            return 2.0 * Math.PI * r;
+        }
+
+        public static double Volume(double r) {
+            return 4.0 / 3.0 * Math.PI * Math.Pow(r, 3.0);
+        }
+    }
+}
---
+++ MembrosEstaticos2/Program.cs
@@ -0,0 +1,12 @@
+﻿using MembrosEstaticos2;
+using System.Globalization;
+
+Console.Write("Entre o valor do raio: ");
+double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double circ = Calculadora.Circunferencia(raio);
+double volume = Calculadora.Volume(raio);
+
+Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+Console.WriteLine("Valor de PI: " + Math.PI.ToString("F2", CultureInfo.InvariantCulture));
